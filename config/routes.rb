4954e6084785896009bc78a3d6d0952c30d0b9b2
Diego Orbezo <diego.orbezoh@gmail.com>
2018-08-26 18:49:21 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/about_us'
+  get 'pages/faq'
+  get 'pages/tos'
+  get 'pages/contact_us'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
